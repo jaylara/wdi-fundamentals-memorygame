ae788ffe4dd1b7109383fa34553eab3846707985
Jay Lara <jay.lara@outlook.com>
2017-08-31 17:48:01 -0500

    ex 9 - created game logic

--- js/main.js
+++ js/main.js
@@ -1,20 +1,27 @@
 console.log("Up and running!");
+
+//array of selectable cards
 var cards = ["queen", "queen", "king", "king"];
+//array of two cards in play
 var cardsInPlay = [];
 
-var cardOne = cards[0];
-var cardTwo = cards[2];
-
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
-
-console.log("User flipped " + cardsInPlay[0]);
-console.log("User flipped " + cardsInPlay[1]);
-
-if(cardsInPlay.length === 2) {
-  if(cardsInPlay[0] === cardsInPlay[1]) {
-    alert("You found a match!!");
-  } else {
-    alert("Sorry, try again!!");
+//check for a match between index 0 and 1 in cardsInPlay
+var checkForMatch = function() {
+  if(cardsInPlay.length === 2) {
+    if(cardsInPlay[0] === cardsInPlay[1]) {
+      alert("You found a match!!");
+    } else {
+      alert("Sorry, try again!!");
+    }
   }
 }
+
+//flips a card 
+var flipCard = function(cardId) {
+  console.log("User flipped "+ cards[cardId]);
+  cardsInPlay.push(cards[cardId]);
+
+  checkForMatch();
+}
+
+flipCard(0);
