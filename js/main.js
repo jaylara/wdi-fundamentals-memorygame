612ad5c1289244da482f508029a0e48a0ba8d403
Jay Lara <jay.lara@outlook.com>
2017-08-30 19:35:45 -0500

    ex 8 - create game logic

--- js/main.js
+++ js/main.js
@@ -1,10 +1,20 @@
- console.log("Up and running!");
- var cardOne = "queen";
- var cardTwo = "queen";
- var cardThree = "king";
- var cardFour = "king";
+console.log("Up and running!");
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
+var cardOne = cards[0];
+var cardTwo = cards[2];
+
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+
+console.log("User flipped " + cardsInPlay[0]);
+console.log("User flipped " + cardsInPlay[1]);
+
+if(cardsInPlay.length === 2) {
+  if(cardsInPlay[0] === cardsInPlay[1]) {
+    alert("You found a match!!");
+  } else {
+    alert("Sorry, try again!!");
+  }
+}
