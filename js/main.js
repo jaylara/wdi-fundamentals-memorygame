c88c2111f0a2215cabb68f264fd3f68d0d8a8964
Jay Lara <jay.lara@outlook.com>
2017-09-05 18:42:32 -0500

    ex 10 - cards array

--- js/main.js
+++ js/main.js
@@ -1,7 +1,28 @@
 console.log("Up and running!");
 
 //array of selectable cards
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+   {
+     rank: "queen",
+     suit: "hearts",
+     cardImage: "images/queen-of-hearts.png"
+   },
+   {
+     rank: "queen",
+     suit: "diamonds",
+     cardImage: "images/queen-of-diamonds.png"
+   },
+   {
+     rank: "king",
+     suit: "hearts",
+     cardImage: "images/king-of-hearts.png"
+   },
+   {
+     rank: "king",
+     suit: "diamonds",
+     cardImage: "images/king-of-diamonds.png"
+   }
+ ];
 //array of two cards in play
 var cardsInPlay = [];
 
@@ -16,12 +37,28 @@ var checkForMatch = function() {
   }
 }
 
-//flips a card 
+//flips a card
 var flipCard = function(cardId) {
-  console.log("User flipped "+ cards[cardId]);
-  cardsInPlay.push(cards[cardId]);
+  console.log("User flipped "+ cards[cardId].rank
+              +" ("+ cards[cardId].suit
+              +", "+ cards[cardId].cardImage);
+  cardsInPlay.push(cards[cardId].rank);
 
   checkForMatch();
 }
 
 flipCard(0);
+flipCard(2);
+
+/* Save for later
+var grungeAlbumsJSON = JSON.stringify(grungeAlbums);
+console.log(grungeAlbumsJSON);
+var grungeAlbumsObject = JSON.parse(grungeAlbumsJSON);
+console.log(grungeAlbumsObject.albums.length);
+for(i = 0; i < grungeAlbumsObject.albums.length; i++) {
+  var currRec = grungeAlbumsObject.albums[i];
+  console.log(currRec.name +" by "
+              + currRec.artist +" sold "
+              + currRec.unitsSold +" units. ");
+}
+*/
