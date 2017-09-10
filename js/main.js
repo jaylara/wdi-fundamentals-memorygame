3faf6a1eebfebcc4ed7b3089a6363642aad2475c
Jay Lara <jay.lara@outlook.com>
2017-09-10 07:34:17 -0500

    ex 11

--- js/main.js
+++ js/main.js
@@ -3,23 +3,19 @@ console.log("Up and running!");
 //array of selectable cards
 var cards = [
    {
-     rank: "queen",
-     suit: "hearts",
+     rank: "queen", suit: "hearts",
      cardImage: "images/queen-of-hearts.png"
    },
    {
-     rank: "queen",
-     suit: "diamonds",
+     rank: "queen", suit: "diamonds",
      cardImage: "images/queen-of-diamonds.png"
    },
    {
-     rank: "king",
-     suit: "hearts",
+     rank: "king", suit: "hearts",
      cardImage: "images/king-of-hearts.png"
    },
    {
-     rank: "king",
-     suit: "diamonds",
+     rank: "king", suit: "diamonds",
      cardImage: "images/king-of-diamonds.png"
    }
  ];
@@ -38,35 +34,26 @@ var checkForMatch = function() {
 }
 
 //flips a card
-var flipCard = function(cardId) {
+var flipCard = function() {
+  var cardId = this.getAttribute('data-id');
   console.log("User flipped "+ cards[cardId].rank
               +" ("+ cards[cardId].suit
               +", "+ cards[cardId].cardImage);
   cardsInPlay.push(cards[cardId].rank);
-
+  this.setAttribute('src', cards[cardId].cardImage);
   checkForMatch();
 }
 
-flipCard(0);
-flipCard(2);
-
-document.querySelectorAll('.current')[1].textContent = "*The Violent Bear It Away";
-document.getElementById('next').textContent = "*Me Talk Pretty One Day";
-document.querySelector('li').textContent = "*Brothers Karamazov";
-document.getElementsByTagName('li')[3].textContent = "*JavaScript is Fun!";
-
-
-
-
-/* Save for later
-var grungeAlbumsJSON = JSON.stringify(grungeAlbums);
-console.log(grungeAlbumsJSON);
-var grungeAlbumsObject = JSON.parse(grungeAlbumsJSON);
-console.log(grungeAlbumsObject.albums.length);
-for(i = 0; i < grungeAlbumsObject.albums.length; i++) {
-  var currRec = grungeAlbumsObject.albums[i];
-  console.log(currRec.name +" by "
-              + currRec.artist +" sold "
-              + currRec.unitsSold +" units. ");
+//creates game board
+var createBoard = function() {
+  for(i = 0; i < cards.length; i++) {
+    var newCardImg = document.createElement('img');
+    newCardImg.setAttribute('data-id', i);
+    newCardImg.setAttribute('src', 'images/back.png');
+    newCardImg.addEventListener('click',flipCard);
+    document.getElementById('game-board').appendChild(newCardImg);
+  }
 }
-*/
+
+/*START*/
+createBoard();
